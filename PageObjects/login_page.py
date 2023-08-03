755af352142bb997d27beb856bc95aa1ba4063e2
TomasGonzz <tomasgonzalez4278@gmail.com>
2023-08-03 13:09:26 -0300

    seguimos con pageobject

---
+++ PageObjects/login_page.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+import base_page
+
+
+class LoginPage(base_page.BasePage):
+    __url = "https://practicetestautomation.com/practice-test-login/"
+    __username_field = (By.NAME, "username")
+    __password_field = (By.ID, 'password')
+    __submit_button = (By.XPATH, "//button[@class='btn']")
+
+    def __init__(self, driver: WebDriver):
+        super().__init__(driver)
+        self._driver = driver
+
+    def open(self):
+        super()._open_url(self.__url)
+
+    def execute_login(self, username: str, password: str):
+        super()._type(self.__username_field, username)
+        super()._type(self.__username_field, password)
+        super()._click(self.__submit_button)
+
+
--- PageObjects/login_success_page.py
+++ PageObjects/login_success_page.py
@@ -1,8 +1,8 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+import base_page
 
-
-class LoggedInSuccessfullyPage:
+class LoggedInSuccessfullyPage(base_page.BasePage):
     __actual_url = "https://practicetestautomation.com/logged-in-successfully/"
     __header = (By.TAG_NAME, "h1")
     __log_out_button = (By.LINK_TEXT, 'Log out')
@@ -10,11 +10,13 @@ class LoggedInSuccessfullyPage:
     def __init__(self, driver: WebDriver):
         self._driver = driver
 
+    @property
     def current_url(self) -> str:
-        return self._driver.current_url
+        return self.
 
     def header(self) -> str:
         return self._driver.find_element(self.__header).text
 
     def is_logout_button_displayed(self) -> bool:
         return self._driver.find_element(self.__log_out_button).is_displayed()
+
